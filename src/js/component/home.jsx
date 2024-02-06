f78aa326033f4f13667437f17bab4a29a618db02
Yeju Lee (Julie) Motley <149555844+yjlmotley@users.noreply.github.com>
2024-02-06 02:59:38 +0000

    added show/hide purple button

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,22 +2,46 @@ import React, { useState } from "react";
 
 
 export function Home()  {
-	const [ selectedColor, setSelectedColor ] = useState("green");
+	const [ selectedColor, setSelectedColor ] = useState("red");
+	const [ showPurple, setShowPurple ] = useState(true);
+
+	const togglePurple = () => {
+        setShowPurple(!showPurple);
+    };
 
 	return (
-		<div className="traffic-light">
-			<div
-				onClick={() => setSelectedColor("red")} 
-				className={"light red" + ((selectedColor === "red") ? " glow" : "")
-				}></div>
-			<div 
-				onClick={() => setSelectedColor("yellow")} 
-				className={"light yellow" + ((selectedColor === "yellow") ? " glow" : "")
-				}></div>
-			<div 
-				onClick={() => setSelectedColor("green")} 			
-				className={"light green" + ((selectedColor === "green") ? " glow" : "")
-				}></div>
+		<div>
+			<span className="purpleButton">
+				<button id="cycleButton" className="btn btn-dark" onClick={togglePurple}>
+					{showPurple ? "Hide Purple Circle1" : "Show Purple Circle1"}
+				</button>
+				<button id="purpleButton" className="btn btn-dark" onClick={togglePurple}>
+					{showPurple ? "Hide Purple Circle" : "Show Purple Circle"}
+				</button>
+			</span>
+			<div className="traffic-light-container">
+			<div className="traffic-light-top"></div>
+			<div className="traffic-light">
+				<div
+					onClick={() => setSelectedColor("red")} 
+					className={"light red" + ((selectedColor === "red") ? " glow" : "")}>
+				</div>
+				<div 
+					onClick={() => setSelectedColor("yellow")} 
+					className={"light yellow" + ((selectedColor === "yellow") ? " glow" : "")}>
+				</div>
+				<div 
+					onClick={() => setSelectedColor("green")} 			
+					className={"light green" + ((selectedColor === "green") ? " glow" : "")}>	
+				</div>
+				{showPurple && (
+                    <div
+                        onClick={() => setSelectedColor("purple")}
+                        className={"light purple" + ((selectedColor === "purple") ? " glow" : "")}
+                    ></div>
+                )}
+			</div>
+			</div>
 		</div>
 	);
 };
